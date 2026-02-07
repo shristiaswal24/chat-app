eb1ccd55596dfc1a231809c30b7c32237b7fe0d5
shristi Aswal <shristiaswal24@gmail.com>
2026-02-07 12:34:23 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Listen for "chatMessage" from a client
+    socket.on('chatMessage', (msg) => {
+        // Broadcast the message to EVERYONE connected
+        io.emit('chatMessage', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
